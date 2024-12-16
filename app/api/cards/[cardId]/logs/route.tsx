17510cd08754cb5386800bbeac71e3b5d436f791
MuradRashidov <muradrashidov995@gmail.com>
2024-12-17 01:06:18 +0400

    postinstall

--- app/api/cards/[cardId]/logs/route.tsx
+++ app/api/cards/[cardId]/logs/route.tsx
@@ -1,15 +1,42 @@
+// import { db } from "@/lib/db";
+// import { auth } from "@clerk/nextjs/server";
+// import { ENTITY_TYPE } from "@prisma/client";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(request: NextRequest, {params}: { params: { cardId: string}}) {
+//     try {
+//         const { orgId, userId } = await auth();
+//         if(!userId || !orgId) return new NextResponse("Unauthorized",{ status: 401 });
+//         const auditLogs = await  db.auditLog.findMany({
+//             where: {
+//                 entityId: params.cardId,
+//                 entityType: ENTITY_TYPE.CARD
+//             },
+//             orderBy: {
+//                 createdAt: "desc"
+//             },
+//             take: 3
+//         });
+//         return NextResponse.json(auditLogs);
+//     } catch (error) {
+//         return new NextResponse("Inteernal error",{ status: 500 });
+
+//     }
+// }
+
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { ENTITY_TYPE } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, {params}: { params: { cardId: string}}) {
+export async function GET(request: NextRequest, context: { params: { cardId: string } }) {
     try {
         const { orgId, userId } = await auth();
-        if(!userId || !orgId) return new NextResponse("Unauthorized",{ status: 401 });
-        const auditLogs = await  db.auditLog.findMany({
+        if (!userId || !orgId) return new NextResponse("Unauthorized", { status: 401 });
+
+        const auditLogs = await db.auditLog.findMany({
             where: {
-                entityId: params.cardId,
+                entityId: context.params.cardId, // params'i burada context üzerinden alıyoruz
                 entityType: ENTITY_TYPE.CARD
             },
             orderBy: {
@@ -17,9 +44,10 @@ export async function GET(request: NextRequest, {params}: { params: { cardId: st
             },
             take: 3
         });
+
         return NextResponse.json(auditLogs);
     } catch (error) {
-        return new NextResponse("Inteernal error",{ status: 500 });
-
+        console.error(error);
+        return new NextResponse("Internal error", { status: 500 });
     }
 }
