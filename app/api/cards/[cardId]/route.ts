0592df7892f0d3d5b11c4df260a386fd62d8d036
MuradRashidov <muradrashidov995@gmail.com>
2024-12-17 00:57:39 +0400

    postinstall

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,12 +1,40 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_:any,{ params }: { params: { cardId?: string } }) {
-    try {
-        console.log("params",params);
+// export async function GET(_:any,{ params }: { params: { cardId?: string } }) {
+//     try {
+//         console.log("params",params);
         
        
+//         const { userId, orgId } = await auth();
+//         if (!orgId || !userId) return new NextResponse('Unauthorized', { status: 401 });
+
+//         const card = await db.card.findUnique({
+//             where: {
+//                 id: params?.cardId,
+//                 list: { board: { orgId } },
+//             },
+//             include: { list: { select: { title: true } } },
+//         });
+//         console.log(card?.title);
+        
+
+//         if (!card) {
+//             return new NextResponse("Card not found", { status: 404 });
+//         }
+
+//         return NextResponse.json(card);
+//     } catch (error) {
+//         console.error(error);
+//         return new NextResponse("Internal Server Error!", { status: 500 });
+//     }
+// }
+
+export async function GET(req: NextRequest, { params }: { params: { cardId: string } }) {
+    try {
+        console.log("params", params);
+
         const { userId, orgId } = await auth();
         if (!orgId || !userId) return new NextResponse('Unauthorized', { status: 401 });
 
@@ -18,7 +46,6 @@ export async function GET(_:any,{ params }: { params: { cardId?: string } }) {
             include: { list: { select: { title: true } } },
         });
         console.log(card?.title);
-        
 
         if (!card) {
             return new NextResponse("Card not found", { status: 404 });
